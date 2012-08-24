f6182e0095c407fafd8d16610dc115eb0bb5650c
Pirhoo <hello@pirhoo.com>
2012-08-24 11:02:33 +0200

    Refactoring

--- test/twitter-mission/index.js
+++ test/twitter-mission/index.js
@@ -1,20 +1,20 @@
 // Dependencies
-var util 	  = require("util")
-	, Mission = require("../../lib/mission");
+var util    = require("util")
+  , Mission = require("../../lib/mission");
 
 function TwitterMission() {
 
-	// Call the parent constructor
-	TwitterMission.super_.call(this);	
+  // Call the parent constructor
+  TwitterMission.super_.call(this);  
 
-	// Here some configuration attributs
-		
-	// On peut changer une variables dont on a hérité (de Mission)
-	this.points 	= 70; // Nombre de points obtenu à la fin de la mission, 100 par defaut
-	this.duration = 20000; // Indique qu'on a 20 secondes pour réaliser la mission, false par defaut (pas de durée)  
+  // Here some configuration attributs
+    
+  // On peut changer une variables dont on a hérité (de Mission)
+  this.points   = 70; // Nombre de points obtenu à la fin de la mission, 100 par defaut
+  this.duration = 20000; // Indique qu'on a 20 secondes pour réaliser la mission, false par defaut (pas de durée)  
 
-	// On peut aussi en ajouter une nouvelle (privée)
-	this.accound_to_find = "@jquest" // Notre mission peut par exemple consister à suivre ce compte
+  // On peut aussi en ajouter une nouvelle (privée)
+  this.accound_to_find = "@jquest" // Notre mission peut par exemple consister à suivre ce compte
 }
 
 /**
@@ -28,16 +28,16 @@ util.inherits(TwitterMission, Mission);
 
 /**
 * Implemente la classe qui va verifier l'état d'avancement de l'utiliseur
-* @param  {Object} 	user 	L'object contenant l'utilisateur
-* @return {Boolean}  	   	Retourne true si la mission est complétée
+* @param  {Object}   user   L'object contenant l'utilisateur
+* @return {Boolean}         Retourne true si la mission est complétée
 */
 TwitterMission.prototype.isCompleted = function(user) {
-	// Des choses à vérifier...
-	// Par exemple, suivont nous le compte indiqué ?
-	// 		on peut écrire une fonction qui va vérifier ça
-	// 		TwitterMission.areFriends(user.nickname, exports.accound_to_find)
+  // Des choses à vérifier...
+  // Par exemple, suivont nous le compte indiqué ?
+  //     on peut écrire une fonction qui va vérifier ça
+  //     TwitterMission.areFriends(user.nickname, exports.accound_to_find)
 
-	return true; // Ou false si on est pas satisfait
+  return true; // Ou false si on est pas satisfait
 };
 
 
